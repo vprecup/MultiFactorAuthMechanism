d5ec779738b860bbdc036606bce24c33fabe7faf
bodeacornelia <bodea_nelly@yahoo.com>
2017-01-24 16:07:33 +0200

    refactor to EmailSender

--- Email/src/main/java/p/EmailTemplate.java
+++ Email/src/main/java/p/EmailTemplate.java
@@ -8,7 +8,6 @@ public class EmailTemplate {
 
     public static String templateGenerator(String token){
 
-        return "<body><h1>Your token was generated:</h1><h2>"+token+"</h2><br><br>"
-                + "<p>To continue click</p><a href='#'>here</a></body>";
+        return "<body><h1>Your token was generated:</h1><h2>"+token+"</h2></body>";
     }
 }
